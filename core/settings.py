fcac06f906d4c3cce8d757369e06ff6a8e223202
dro14 <doniyorbek752@gmail.com>
2023-10-03 14:35:02 +0500

    edit configs for serving static files

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,8 @@ AUTH_USER_MODEL = "alpha.User"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
