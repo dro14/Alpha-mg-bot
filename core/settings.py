e10c391a3bde409fe396584a1ae548e4a63ef1ac
dro14 <doniyorbek752@gmail.com>
2023-10-03 14:48:53 +0500

    install whitenoise to collect static files

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    os.environ["HOST_DOMAIN"],
-]
+ALLOWED_HOSTS = ["localhost", os.environ["HOST_DOMAIN"]]
 
 
 # Application definition
@@ -53,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -121,7 +119,6 @@ AUTH_USER_MODEL = "alpha.User"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
