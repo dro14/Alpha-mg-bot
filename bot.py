ded3f9d58e722136149fc2b70376fee801b71853
dro14 <doniyorbek752@gmail.com>
2023-10-06 07:05:45 +0500

    connect bot to db

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from alpha.models import CustomUser
 from pyrogram import Client, filters
 import os
 
@@ -11,7 +12,14 @@ app = Client(
 
 @app.on_message(filters.incoming & filters.private & filters.text)
 def handle_text(client, message):
-    message.reply_text(message.text)
+    if message.text == "/users":
+        users = CustomUser.objects.all()
+        reply = ""
+        for i, user in enumerate(users):
+            reply += f"{i+1}. {user.username}\n"
+        message.reply_text(reply)
+    else:
+        message.reply_text(message.text)
 
 
 if __name__ == "__main__":
