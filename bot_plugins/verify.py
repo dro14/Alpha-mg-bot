849580b6ee062294b25e89395ad80aa5ff322b2f
dro14 <doniyorbek752@gmail.com>
2023-10-12 09:21:53 +0500

    change found_match function from sync to async

--- bot_plugins/verify.py
+++ bot_plugins/verify.py
@@ -2,7 +2,7 @@ from alpha.models import User, CustomUser
 from pyrogram import filters
 
 
-async def found_match(users, update, attr1, attr2):
+def found_match(users, update, attr1, attr2):
     for user in users:
         if getattr(user, attr1) == getattr(update.from_user, attr1):
             if not user.user_id:
@@ -18,10 +18,10 @@ async def func(_, client, update):
         for model in [CustomUser, User]:
             users = model.objects.all()
             if update.from_user.username:
-                if await found_match(users, update, "username", "phone_number"):
+                if found_match(users, update, "username", "phone_number"):
                     return True
             if update.from_user.phone_number:
-                if await found_match(users, update, "phone_number", "username"):
+                if found_match(users, update, "phone_number", "username"):
                     return True
 
     await client.send_message(update.from_user.id, "Вы не зарегистрированы в системе")
