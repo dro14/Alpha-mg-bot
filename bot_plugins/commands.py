cacd48895e803bc89ae9b430349b44d906952dd8
dro14 <doniyorbek752@gmail.com>
2023-10-29 19:03:26 +0500

    add cancel command

--- bot_plugins/commands.py
+++ bot_plugins/commands.py
@@ -20,6 +20,16 @@ def start(_, message):
         message.reply(start_sender_receiver)
 
 
+@Client.on_message(filters.command("admin") & registered & admin_verbose)
+def admin(_, message):
+    button_text = "Зайти в админ панель"
+    url = "https://" + os.environ["HOST_DOMAIN"] + "/admin/"
+    button = InlineKeyboardButton(button_text, url=url)
+    reply_markup = InlineKeyboardMarkup([[button]])
+
+    message.reply(admin_text, reply_markup=reply_markup)
+
+
 @Client.on_message(filters.command("send") & registered & sender_verbose)
 def send(_, message):
     user_data = {"current": "cargo_type"}
@@ -31,11 +41,10 @@ def send(_, message):
     message.reply(send_text, reply_markup=reply_markup)
 
 
-@Client.on_message(filters.command("admin") & registered & admin_verbose)
-def admin(_, message):
-    button_text = "Зайти в админ панель"
-    url = "https://" + os.environ["HOST_DOMAIN"] + "/admin/"
-    button = InlineKeyboardButton(button_text, url=url)
-    reply_markup = InlineKeyboardMarkup([[button]])
-
-    message.reply(admin_text, reply_markup=reply_markup)
+@Client.on_message(filters.command("cancel") & registered & sender_verbose)
+def cancel(_, message):
+    redis.delete(f"user:{message.from_user.id}")
+    redis.delete(f"photo_count:{message.from_user.id}")
+    for i in range(1, 5):
+        redis.delete(f"photo_{i}:{message.from_user.id}")
+    message.reply(cancel_text)
